6335fb2833a428c41f97149093cddfc130c58c03
Grupa 2 Java <seinho_>
2019-02-05 18:06:39 +0200

    Initial commit

---
+++ src/scene/MyScene.java
@@ -0,0 +1,24 @@
+package scene;
+
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyScene extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello");
+		VBox vb = new VBox();
+		Button btn = new Button("Hello my son");
+		vb.getChildren().add(btn);
+		Scene scene = new Scene(vb);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
